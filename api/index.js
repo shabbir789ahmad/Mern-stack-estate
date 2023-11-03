b69b591db765268de67fd289feeaecd668d16487
shabbir ahmad <shabbir789shahid@gmail.com>
2023-11-03 09:28:59 +0500

    Create user model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
+// C9wqjme16DChB8gm
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('connected')
+}).catch(()=>{
+    console.log('fail to conect to database')
+})
 const app =express();
 
 app.listen(3000, ()=>{
