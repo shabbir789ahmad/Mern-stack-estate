e83526036714f5a122040e9b92ed02b7f51683e2
shabbir ahmad <shabbir789shahid@gmail.com>
2023-11-14 18:21:57 +0500

    sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
+import { json } from 'express';
 import User from '../models/user.modal.js'
 import {errorHandler} from '../utils/error.js'
 import bcryptjs from 'bcryptjs';
-
+import  jwt from "jsonwebtoken";
 
 
 export const SignUp=async (req, res, next) =>{
@@ -24,7 +25,12 @@ export const LogIn=async (req, res, next) =>{
      try{
           const validUser=await User.findOne({email:email});
           if(!validUser) return next(errorHandler(404, "User not found!"))
-          // res.status(201).json('User created successfully!');
+          const validPassword = bcryptjs.compareSync(password, validUser.password);
+          if(!validPassword) return next(errorHandler(401, "These creddentilas does not match our records!"))
+          const token = jwt.sign(validUser._id, process.env.JWT_SECRET)
+          const {password:pass, ...rest} = validUser._doc;
+          res.cookie('access_token',token, {httpOnly:true}).status(200)
+          .json(rest);
      }catch(error)
      {
           next(error)
